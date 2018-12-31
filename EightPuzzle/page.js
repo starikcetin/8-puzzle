194caebef8d1bb659f1f7768a8e89d27ade9b3b9
starikcetin <cetinsamedtarik@gmail.com>
2019-01-01 02:09:10 +0300

    basic looping

--- EightPuzzle/page.js
+++ EightPuzzle/page.js
@@ -1,3 +1,11 @@
-$(function(){
-    
+$(function () {
+    var homeContentTimer = setInterval(function () {
+        $("#homeContent").children().each(function (index) {
+            if ($(this).hasClass("hide") === false) {
+                $(this).addClass("hide");
+                $(this).nextCircular().removeClass("hide");
+                return false;
+            }
+        });
+    }, 2000);
 });
